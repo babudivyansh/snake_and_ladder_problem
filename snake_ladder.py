6428fc03044082ceed0227229abed321caea0054
babudivyansh <divyansh.verma525@gmail.com>
2023-12-01 12:31:50 +0530

    [Divyansh Babu] Add: function for The Player rolls the die to get a number between 1 and 6

--- snake_ladder.py
+++ snake_ladder.py
@@ -1,14 +1,15 @@
 """
 @Author: Divyansh Babu
 
-@Date: 2021-12-01 11:55
+@Date: 2021-12-01 12:30
 
 @Last Modified by: Divyansh Babu
 
-@Last Modified time: 2021-12-01 11:55
+@Last Modified time: 2021-12-01 12:30
 
 @Title : Snake and Ladder Problem.
 """
+import random
 
 
 def single_ply_at_pos_0():
@@ -23,5 +24,18 @@ def single_ply_at_pos_0():
     return 0
 
 
+def roll_die_1_to_6():
+    """
+        Description: implementation of function for The Player rolls the die to get a number between 1 and 6.
+
+        Parameter: None
+
+        Return: position result
+    """
+    result = random.randint(1, 6)
+    return result
+
+
 if __name__ == '__main__':
-    single_ply_at_pos_0()
+    print(single_ply_at_pos_0())
+    print(roll_die_1_to_6())
