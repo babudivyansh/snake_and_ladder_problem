397f99d9e6384ed6afb7f8485078f7b4191b61e8
babudivyansh <divyansh.verma525@gmail.com>
2023-12-01 11:59:00 +0530

    [Divyansh Babu] Add: [200~Snake and Ladder

---
+++ snake_ladder.py
@@ -0,0 +1,27 @@
+"""
+@Author: Divyansh Babu
+
+@Date: 2021-12-01 11:55
+
+@Last Modified by: Divyansh Babu
+
+@Last Modified time: 2021-12-01 11:55
+
+@Title : Snake and Ladder Problem.
+"""
+
+
+def single_ply_at_pos_0():
+    """
+        Description: implementation of Snake and Ladder game played with single player at start position 0.
+
+        Parameter: None
+
+        Return: 0 position
+    """
+
+    return 0
+
+
+if __name__ == '__main__':
+    single_ply_at_pos_0()
